f67d36fb2783090fcef1f77847bb62a97509be02
x-cyber@windowslive.com <x-cyber@windowslive.com>
2019-12-24 20:52:43 +0700

    go

--- Pages/Counter.razor.cs
+++ Pages/Counter.razor.cs
@@ -1,23 +1,15 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.Connections.Features;
+using Microsoft.AspNetCore.Internal;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Protocol;
 
 public class TestHub : Hub
 {
     public async Task ReceiveMessage(int v)
     {
         await Clients.All.SendAsync("ReceiveMessage", v);
-    }
-
-    public override Task OnConnectedAsync()
-    {
-        Console.WriteLine("Connected");
-        return base.OnConnectedAsync();
-    }
-
-    public override async Task OnDisconnectedAsync(Exception e)
-    {
-        Console.WriteLine($"Disconnected {e?.Message}");
-        await base.OnDisconnectedAsync(e);
     }
 }
